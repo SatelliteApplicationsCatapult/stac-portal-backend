464b421e6b4a991f7d73015b1ca1db6b114ef4b1
James <james.hinton@live.co.uk>
2022-10-11 12:41:35 +0100

    added support for automatic stac creation

--- app/main/service/stac_generator_service.py
+++ app/main/service/stac_generator_service.py
@@ -1,78 +1,153 @@
 from datetime import datetime
 
 import pystac
-from rasterio.warp import calculate_default_transform
-from shapely.geometry import Polygon, mapping
-from rasterio.warp import calculate_default_transform, transform_bounds
-import pyproj
+from rasterio.warp import transform_bounds
+from shapely.geometry import Polygon
 from pyproj import CRS
-import json
-import uuid
-
+import re
 
 def create_STAC_Item(metadata):
-    geom = return_geom_from_multiple_geom(metadata["groupedVariables"]["geom"])
+    # EPSG (Source and destination)
     src_crs = return_epsg_from_wkt(metadata["staticVariables"]["wkt"])
-    destination_crs = "epsg:4326"
+    destination_crs = "epsg:4326"  # All STAC Items are in EPSG:4326
 
-    bbox = return_bbox_from_geom(geom, src_crs, destination_crs)
-    geom = return_geom_from_bbox(bbox)
+    # Converted Geoms
+    wgs84_grouped_geom = return_geom_from_multiple_geom(
+        metadata["groupedVariables"]["wgs84_geom"]
+    )
+    wgs84_bbox = return_bbox_from_geom(wgs84_grouped_geom, src_crs, destination_crs)
+    wgs84_geom = return_geom_from_bbox(wgs84_bbox)
 
+    # Original Geoms
+    original_grouped_geom = return_geom_from_multiple_geom(
+        metadata["groupedVariables"]["proj_geom"]
+    )
+    original_bbox = return_bbox_from_coordinates(
+        original_grouped_geom, src_crs, src_crs
+    )
+
+    # Required fields
     id = metadata["staticVariables"]["id"]
     time_acquired = datetime.strptime(
         metadata["staticVariables"]["time_acquired"], "%Y-%m-%dT%H:%M:%S.%fZ"
     )
 
-    # # Instantiate pystac item
+    # Properties
+    properties = {
+        "proj:bbox": original_bbox,
+        "proj:epsg": src_crs,
+    }
+    # Call the appropriate parser to extend the properties
+    if metadata["staticVariables"]["provider"] == "Planet":
+        planet_stac_parser(properties, metadata["additional"])
+
+    # Instantiate pystac item
     item = pystac.Item(
-        id=id, geometry=geom, bbox=bbox, datetime=time_acquired, properties={}
+        id=id,
+        geometry=wgs84_geom,
+        bbox=wgs84_bbox,
+        datetime=time_acquired,
+        properties=properties,
     )
 
     for asset in metadata["assets"]:
-        random_id = str(uuid.uuid4())
+        # Remove extension from asset name
+        name = re.sub(r"\.[^.]*$", "", asset["filename"])
         item.add_asset(
-            key=random_id,
+            key=name,
             asset=pystac.Asset(
                 href=asset["href"],
-                media_type=asset["type"],
+                media_type=asset["type"],  # TODO: Convert to pystac.MediaType
                 extra_fields={
-                    "shape": asset["shape"],
-                    "transform": asset["transform"],
+                    "eo:bands": asset["bands"],
+                    "proj:shape": asset["shape"],
+                    "proj:transform": asset["transform"],
                 },
             ),
         )
-    return item
+
+    # Add STAC extensions
+    item.stac_extensions = [
+        "https://stac-extensions.github.io/eo/v1.0.0/schema.json",
+        "https://stac-extensions.github.io/projection/v1.0.0/schema.json",
+        "https://stac-extensions.github.io/view/v1.0.0/schema.json",
+    ]
+
+    item.set_self_href("item.json")
+
+    item.validate()
+    item_json = item.save_object()
+    return item_json
 
 
 def return_geom_from_multiple_geom(geom):
     """Return a single geometry from a list of geometries"""
-    if len(geom) == 1:
-        return geom[0]
-    else:
-        return geom
+    # For now, just return the first geom and convert to geometry obect
+    return geom[0]
 
 
 def return_bbox_from_geom(geom, src_crs, destination_crs):
-    """Return a bbox from a geometry"""
+    """
+    Return a bbox from a geometry
+    """
+    geom = Polygon(geom["coordinates"][0])
     left, bottom, right, top = transform_bounds(
-        src_crs,
-        destination_crs,
-        geom[0]["lowerLeft"][0],
-        geom[0]["lowerLeft"][1],
-        geom[0]["lowerRight"][0],
-        geom[0]["upperLeft"][1],
+        "epsg:4326", destination_crs, *geom.bounds
     )
     return [left, bottom, right, top]
 
 
 def return_geom_from_bbox(bbox):
     """Return a geometry from a bbox"""
-    return Polygon(
-        [[bbox[0], bbox[1]], [bbox[0], bbox[3]], [bbox[2], bbox[3]], [bbox[2], bbox[1]]]
-    )
+    return {
+        "type": "Polygon",
+        "coordinates": [
+            [
+                [bbox[0], bbox[1]],
+                [bbox[2], bbox[1]],
+                [bbox[2], bbox[3]],
+                [bbox[0], bbox[3]],
+                [bbox[0], bbox[1]],
+            ]
+        ],
+    }
 
 
 def return_epsg_from_wkt(wkt):
     """Return an EPSG code from a WKT"""
     src_crs = CRS.from_wkt(wkt)
     return src_crs.to_epsg()
+
+
+def return_bbox_from_coordinates(geom, src_crs, destination_crs):
+    """Return a bbox from mapped coorindates"""
+    left, bottom, right, top = transform_bounds(
+        src_crs,
+        destination_crs,
+        geom["lowerLeft"][0],
+        geom["lowerLeft"][1],
+        geom["lowerRight"][0],
+        geom["upperLeft"][1],
+    )
+    return [left, bottom, right, top]
+
+
+def planet_stac_parser(properties, metadata):
+    """Parse Planet STAC metadata"""
+    print("Parsing Planet STAC metadata")
+    planet_properties = metadata["properties"]
+    if planet_properties.get("gsd"):
+        properties["gsd"] = planet_properties["gsd"]
+    
+    # EO Additions
+    if planet_properties.get("cloud_cover") != None:
+        print('YO')
+        properties["eo:cloud_cover"] = planet_properties["cloud_cover"]
+
+    # View Additions
+    if planet_properties.get("sun_elevation"):
+        properties["view:sun_elevation"] = planet_properties["sun_elevation"]
+    if planet_properties.get("sun_azimuth"):
+        properties["view:sun_azimuth"] = planet_properties["sun_azimuth"]
+    if planet_properties.get("view_angle"):
+        properties["view:off_nadir"] = planet_properties["view_angle"]
