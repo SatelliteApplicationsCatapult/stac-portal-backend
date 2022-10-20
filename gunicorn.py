d3021f0dbd42b6e24de7a6c5b28e1479637092f1
Ivica <ivica-matic@outlook.com>
2022-10-20 17:36:22 +0100

    Changed port to 5001

--- gunicorn.py
+++ gunicorn.py
@@ -1,6 +1,6 @@
 # create a gunicorn config file
 import multiprocessing
-bind = "0.0.0.0:5000"
+bind = "0.0.0.0:5001"
 workers = (multiprocessing.cpu_count() * 2) + 1
 threads = workers
 worker_class = "gevent"
